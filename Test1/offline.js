ce346cb3250829db6a93e3f7cb1d7fc3e27f8f15
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2019-06-13 17:09:26 -0400

    Roy!

---
+++ Test1/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1560460070,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/solidground-sheet0.png",
+		"images/roy-sheet0.png",
+		"images/paltfrom-sheet0.png",
+		"media/bgm.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
